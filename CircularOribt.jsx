3a99bffb59b3a65d78ffcc91f339c108d34196a0
Ghias Ali <76915661+GhiasAli17@users.noreply.github.com>
2024-06-09 23:30:12 +0500

    Created CircularOribt of small circle on the boundary of large circle

---
+++ CircularOribt.jsx
@@ -0,0 +1,46 @@
+export default function CircularOrbit(){
+
+return (
+<div class="flex justify-center items-center min-h-screen">
+
+
+<div class="size-[350px] relative bg-white border-slate-500 border rounded-full flex justify-center items-center ">
+  <div class="size-[224px] relative bg-white border-slate-500 border rounded-full ">
+  <div>
+  <div class="size-10 left-0 top-11 bg-red-600 absolute rounded-full">
+
+  </div>
+   <div class="size-10 -top-5 left-[40%] bg-slate-600 absolute rounded-full">
+</div>
+
+ <div class="size-10  right-0 top-11 bg-slate-600 absolute rounded-full">
+</div>
+
+<div class="size-10  -right-4 top-[150px] bg-slate-900 absolute rounded-full">
+</div>
+<div class="size-10  -left-4 top-[150px] bg-slate-900 absolute rounded-full">
+</div>
+<div class="size-10  left-[40%] -bottom-5 bg-slate-900 absolute rounded-full">
+</div>
+</div>
+</div>
+  <div>
+  <div class="size-10 left-0 top-11 bg-red-600 absolute rounded-full">
+
+  </div>
+   <div class="size-10 -top-5 left-[40%] bg-slate-600 absolute rounded-full">
+</div>
+
+ <div class="size-10  right-0 top-11 bg-slate-600 absolute rounded-full">
+</div>
+
+<div class="size-10  -right-4 top-[150px] bg-slate-900 absolute rounded-full">
+</div>
+<div class="size-10  -left-4 top-[150px] bg-slate-900 absolute rounded-full">
+</div>
+<div class="size-10  left-[40%] -bottom-5 bg-slate-900 absolute rounded-full">
+</div>
+</div>
+</div>
+</div>
+)}
